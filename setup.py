1062beb6f56af427814b33f446b0773337822689
Antoine Beyeler <abeyeler@ab-ware.com>
2022-04-18 17:48:07 +0200

    Bumped version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="hatched",
-    version="0.1.0a2",
+    version="0.1.0",
     description="Convert images to plotter-friendly hatched patterns",
     long_description=readme,
     long_description_content_type="text/markdown",
